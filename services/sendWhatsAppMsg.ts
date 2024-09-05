523844e178f5fc3742c14ef99f5fa280bd58611e
Yousef Qawasmeh <qwasme.yousef@gmail.com>
2024-09-05 12:57:10 +0300

    fix whatsapp install chrome in linx container

--- services/sendWhatsAppMsg.ts
+++ services/sendWhatsAppMsg.ts
@@ -3,6 +3,12 @@ const qrcode = require('qrcode-terminal');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
+    puppeteer: {
+      headless: true,
+      args: [
+        '--no-sandbox',
+      ],
+    }
 });
 
 client.on('qr', (qr: any) => {
