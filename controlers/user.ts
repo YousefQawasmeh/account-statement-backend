51505b780812496f63257e242a6950d3fa85b730
Yousef Qawasmeh <qwasme.yousef@gmail.com>
2025-03-19 19:09:43 +0200

    feat: auto check Users With Mismatched Total reminder cron job

--- controlers/user.ts
+++ controlers/user.ts
@@ -66,4 +66,15 @@ export const createUser = async (data: any) => {
   }
 }
 
+export const getUsersWithMismatchedTotal = async () => {
+  const usersWithMismatchedTotal = await User.createQueryBuilder('user')
+    .innerJoin('user.records', 'record')
+    .select('user.id, user.cardId, user.name, user.subName, user.total, SUM(record.amount) as totalRecordsAmount')
+    .groupBy('user.id')
+    .having('SUM(record.amount) <> user.total')
+    .getRawMany();
+
+  return usersWithMismatchedTotal
+}
+
 export default { createUser, getNewCardId }
