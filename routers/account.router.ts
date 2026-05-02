ffbb03eeef474990fa38f778dab030c4b5c8dc4f
YousefQawasmeh <48014915+YousefQawasmeh@users.noreply.github.com>
2026-05-02 21:23:40 +0300

    feat: add change own password endpoint

--- routers/account.router.ts
+++ routers/account.router.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register, getMe, getAllAccounts, updateAccount, deleteAccount } from "../controlers/account.js";
+import { login, register, getMe, getAllAccounts, updateAccount, deleteAccount, changeMyPassword } from "../controlers/account.js";
 import { authenticate, adminOnly, anyRole } from "../middleware/auth.js";
 
 const router = express.Router();
@@ -7,6 +7,8 @@ const router = express.Router();
 router.post("/login", login);
 
 router.get("/me", authenticate, anyRole, getMe);
+
+router.put("/me/password", authenticate, anyRole, changeMyPassword);
 
 router.post("/register", authenticate, adminOnly, register);
 
